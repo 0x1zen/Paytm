342d3e925d22e9bbd25ea8914fe81525d66114c4
0x1zen <rajdubalwork@gmail.com>
2024-03-07 20:20:19 +0530

    WIP:Adding a /me route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,15 @@
 const express = require('express');
 const userRouter = require("./user");
 const accountRouter = require("./account");
+const { authMiddleware } = require('../middleware');
 
 const router = express.Router();
+
+router.use('/me',authMiddleware,(req,res)=>{
+    res.status(200).json({
+        authentication:true
+    })
+})
 
 router.use("/user", userRouter);
 router.use("/account", accountRouter);
