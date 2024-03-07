8335e3534c6320b07ca5e9a2345a8dcba203e8e9
0x1zen <rajdubalwork@gmail.com>
2024-03-07 20:17:52 +0530

    Fixed account balance not showing

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from "react";
 import { Appbar } from "../components/Appbar"
 import { Balance } from "../components/Balance"
 import { Users } from "../components/Users"
-
+import axios from "axios";
 
 export const Dashboard = () => {
+    const [balance,setBalance]=useState(null);
+    useEffect(()=>{
+        async function fetchBalance(){
+                const response = await axios.get("http://localhost:3000/api/v1/account/balance",{
+                    headers:{
+                        Authorization:"Bearer "+ localStorage.getItem("token")
+                    }
+                });
+                setBalance(response.data.balance); 
+        }
+        fetchBalance();
+    },[]);
     return <div>
         <Appbar />
         <div className="m-8">
-            <Balance value={"10,000"} />
+            <Balance value={balance} />
             <Users></Users>
         </div>
     </div>
