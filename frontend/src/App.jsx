13ec51a5c109a9c96ae9f99c1fe4168aff61489e
0x1zen <rajdubalwork@gmail.com>
2024-03-25 10:54:41 +0530

    Ready to host

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,50 +2,36 @@ import {
   BrowserRouter,
   Route,
   Routes,
-  useNavigate,
   Navigate
 } from "react-router-dom";
-
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Dashboard } from "./pages/Dashboard";
 import { SendMoney } from "./pages/SendMoney";
-import { useEffect,useState } from "react";
-import axios from "axios";
+import { useEffect } from "react";
 
 
 function App() {
-  // const [isAuthenticated,setIsAuthenticated]=useState(false);
-  // useEffect(()=>{
-  //   async function fetch(){
-  //     const response=await axios.get("http://localhost:3000/api/v1/me",{
-  //     headers:{
-  //       Authorization:"Bearer "+ localStorage.getItem("token")
-  //     }
-  //   })
-  //   if(response.data.authentication){
-  //     setIsAuthenticated(true);
-  //   }
-  //   }
-  //   fetch(); 
-  // },[])
+  const token = localStorage.getItem('token');
+  useEffect(() => {
+    if (token && window.location.pathname !== '/dashboard') {
+
+      window.location.href = '/dashboard';
+    }
+  }, []);
+
   return (
-    <>
-       <BrowserRouter>
-       {/* {isAuthenticated ? (
-          <Navigate to="/dashboard" />
-        ) : (
-          <Navigate to="/signin" />
-        )} */}
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<SendMoney />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+        <Route path="*" element={<Navigate to="/signin" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
